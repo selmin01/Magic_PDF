7cb0e4613c8c7e6da6a9fd1773fd157ca4a4750d
‘selmin01’ <anselmogabriel421@gmail.com>
2023-08-02 20:34:18 -0300

    legal uma melhorada

--- magic_pdf/resources/views/index.blade.php
+++ magic_pdf/resources/views/index.blade.php
@@ -7,6 +7,7 @@
         <title>Document</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+        <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
@@ -21,9 +22,12 @@
     </head>
     <body>
         <div class="banner">
-            <h1>Aliança Geek com Pedro Souza</h1>
-            <p>Toda quarta-feira às 20:30h!</p>
-            
+            <h1>Export aqui seu PDF</h1>
+            <p>Mil Grau!</p>
+        </div>
+        <div class="frase-de-impacto">
+            <!-- <p>Quando você acredita em si mesmo, o mundo acredita em você.</p> -->
+            <h4>O futuro pertence àqueles que desafiam o status e abraçam a mudança com ousadia e criatividade.</h4>
         </div>
         <div class="style_body">
             <div class="container">
@@ -58,6 +62,10 @@
                 </div>
             </div>
         </div>
+
+        <footer class="footer">
+            <p>Desenvolvido por <a href="mailto:seuemail@example.com">Gabriel Anselmo Ramos</a></p>
+        </footer>
 
         <!-- Adicione o script do Bootstrap no final do arquivo -->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
