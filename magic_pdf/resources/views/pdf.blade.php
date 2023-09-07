6a104784cf1498812ab660c7343c69db2e4d00d3
‘selmin01’ <anselmogabriel421@gmail.com>
2023-09-07 14:23:34 -0300

    topp

--- magic_pdf/resources/views/pdf.blade.php
+++ magic_pdf/resources/views/pdf.blade.php
@@ -9,6 +9,17 @@
         .test{
             color: #7b00ff;
         }
+        .minha-div {
+            margin: 20px; /* Margens em todos os lados (topo, direita, baixo, esquerda) de 20 pixels */
+            padding: 10px; /* Preenchimento em todos os lados de 10 pixels */
+            background-color: #f0f0f0; /* Cor de fundo para destacar a div */
+        }
+        .minha-classe-de-paragrafo {
+            margin-top: 10px; /* Margem superior de 10 pixels para o parágrafo */
+            margin-bottom: 10px; /* Margem inferior de 10 pixels para o parágrafo */
+            margin-left: 5px; /* Margem esquerda de 5 pixels para o parágrafo */
+            margin-right: 5px; /* Margem direita de 5 pixels para o parágrafo */
+        }
     </style>
 </head>
 <body>
@@ -16,5 +27,10 @@
         <h1>Exemplo de PDF com Blade</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
     </div>
+
+    <div class="minha-div">
+        <p class="minha-classe-de-paragrafo">Este é um parágrafo com margens.</p>
+    </div>
+
 </body>
 </html>
